836472ef5c67cb22c2d86e8591e6dd4dae385dfe
duong-sau <duongsau1211@gmail.com>
2021-10-14 00:31:52 +0700

    [ds]

---
+++ Model/Result/STDShow.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pathlib import Path
+path = 'positive-freeze'
+
+result = pd.DataFrame()
+files = Path(path).glob('*_test.csv')
+for file in files:
+    df = pd.read_csv(file, header=0)
+    result_new = df['similarity'].value_counts()
+    result = result.append(result_new)
+    result.describe()
--- Model/Result/positive-freeze/sho.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("44.0_test.csv", header = 0)
-df.std
