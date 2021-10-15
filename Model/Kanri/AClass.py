ec93297bad79acbb1f2a70033bbf910f6f94fd33
duong-sau <duongsau1211@gmail.com>
2021-10-15 20:58:32 +0700

    [chatbot]

--- Model/Kanri/AClass.py
+++ Model/Kanri/AClass.py
@@ -5,7 +5,7 @@ from Model.Common import toT5sentence, train_validate_test_split
 from Static.Define import path_common
 
 
-sentence_df = pd.read_csv(path_common.sentence.value)
+sentence_df = pd.read_csv(path_common.train.value)
 
 sentence_df['sentence'] = 'text classification: ' + sentence_df['sentence']
 sentence_df['intent_group_index'] = sentence_df['intent_group_index'] + 10
