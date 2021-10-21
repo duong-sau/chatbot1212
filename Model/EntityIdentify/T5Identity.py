605e221abd2331ef1849c20d3dea94b1109fdc1d
duong-sau <duongsau1211@gmail.com>
2021-10-21 22:03:52 +0700

    [chatbot] freeze num layer

--- Model/EntityIdentify/T5Identity.py
+++ Model/EntityIdentify/T5Identity.py
@@ -8,39 +8,40 @@ from Static.Define import path_common
 import time
 
 # define
-name = 'T5-Small'
+#names = ['freeze1', 'freeze2', 'freeze3', 'freeze4', 'freeze5']
+names= ['freeze0', 'freeze6']
+for name in names:
+    tokenizer = T5Tokenizer.from_pretrained(MODEL['name'])
+    tokenConfig(tokenizer=tokenizer)
+    config = T5Config.from_pretrained(MODEL['name'])
+    # config.num_decoder_layers = MODEL['num_decoder_layers']
+    model = T5ForConditionalGeneration.from_pretrained('../Save/Try/' + name + "/", config=config)
+    model.cpu()
 
-tokenizer = T5Tokenizer.from_pretrained(MODEL['name'])
-tokenConfig(tokenizer=tokenizer)
-config = T5Config.from_pretrained(MODEL['name'])
-# config.num_decoder_layers = MODEL['num_decoder_layers']
-model = T5ForConditionalGeneration.from_pretrained('../Save/Try/' + name + "/", config=config)
-model.cpu()
+    test_link = "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/Data/IntentClassification/test.csv"
 
-test_link = "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/Data/IntentClassification/test.csv"
-
-test_df = pd.read_csv(test_link, header=0)
-columns = ["test_id", "expected", "actual", "max2", "max3"]
-result_df = pd.DataFrame(columns=columns)
-tqdm.pandas()
-for index, row in tqdm(test_df.iterrows(), leave=False, total=len(result_df)):
-    temp_df = pd.read_csv(
-        "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/Data/IntentClassification/sentence_list.csv",
-        header=0)
-    test_sentence = row["sentence"]
-    for i, r in temp_df.iterrows():
-        compare_sentences = r["sentence"]
-        similarity = getSimilarity(tokenizer=tokenizer, model=model, test_sentence=test_sentence,
-                                   compare_sentences=compare_sentences)
-        temp_df.loc[i, "similarity"] = similarity
-    temp_df['similarity'] = pd.to_numeric(temp_df['similarity'], errors='coerce')
-    path_name = "../Result/" + name + "/" + str(row['sentence_index']) + "_test.csv"
-    temp_df.to_csv(path_name, index=0)
-    mean_df = temp_df.groupby(["intent_index"])["similarity"].mean().reset_index().sort_values("similarity")
-    max1 = mean_df.iloc[-1]
-    max2 = mean_df.iloc[-2]
-    max3 = mean_df.iloc[-3]
-    new_row = {'test_id': row["sentence_index"], 'expected': row["intent_index"], 'actual': max1["intent_index"],
-               'max2': max2["intent_index"], 'max3': max3["intent_index"]}
-    result_df = result_df.append(new_row, ignore_index=True)
-result_df.to_csv(path_or_buf='../Result/' + name + '/test_identity_result.csv', mode='w', index=False)
+    test_df = pd.read_csv(test_link, header=0)
+    columns = ["test_id", "expected", "actual", "max2", "max3"]
+    result_df = pd.DataFrame(columns=columns)
+    tqdm.pandas()
+    for index, row in tqdm(test_df.iterrows(), leave=False, total=len(result_df)):
+        temp_df = pd.read_csv(
+            "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/Data/IntentClassification/sentence_list.csv",
+            header=0)
+        test_sentence = row["sentence"]
+        for i, r in temp_df.iterrows():
+            compare_sentences = r["sentence"]
+            similarity = getSimilarity(tokenizer=tokenizer, model=model, test_sentence=test_sentence,
+                                       compare_sentences=compare_sentences)
+            temp_df.loc[i, "similarity"] = similarity
+        temp_df['similarity'] = pd.to_numeric(temp_df['similarity'], errors='coerce')
+        path_name = "../Result/" + name + "/" + str(row['sentence_index']) + "_test.csv"
+        temp_df.to_csv(path_name, index=0)
+        mean_df = temp_df.groupby(["intent_index"])["similarity"].mean().reset_index().sort_values("similarity")
+        max1 = mean_df.iloc[-1]
+        max2 = mean_df.iloc[-2]
+        max3 = mean_df.iloc[-3]
+        new_row = {'test_id': row["sentence_index"], 'expected': row["intent_index"], 'actual': max1["intent_index"],
+                   'max2': max2["intent_index"], 'max3': max3["intent_index"]}
+        result_df = result_df.append(new_row, ignore_index=True)
+    result_df.to_csv(path_or_buf='../Result/' + name + '/test_identity_result.csv', mode='w', index=False)
--- Model/Result/IntentClassificationShow.py
+++ Model/Result/IntentClassificationShow.py
@@ -1,32 +1,42 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
+import numpy as np
 from Static.Define import path_common, Colors
-
-name = 'T5-Small'
-
-result_df = pd.read_csv(name + '/test_identity_result.csv', header=0)
-match = 0
-not_match = 0
-for i, row in result_df.iterrows():
-    if int(row['expected'] == row['actual']):
-        match = match + 1
-    else:
-        not_match += 1
-print(f"for negative method")
-print(f" {Colors.OKGREEN}match case    :{match}")
-print(f"{Colors.WARNING}not match case: {not_match} \n")
-
-intent_df = pd.read_csv(path_common.intent_list.value, header=0)
-
-result_df = pd.read_csv(name + '/test_identity_result.csv', header=0)
-match = 0
-not_match = 0
-for i, row in result_df.iterrows():
-    if row['expected'] == row['actual'] or row['expected'] == row['max2'] or row['expected'] == row['max3']:
-        match = match + 1
-    else:
-        not_match += 1.5
-print(f"{Colors.ENDC}for positive method")
-print(f"{Colors.OKGREEN}match case    :{match}")
-print(f"{Colors.WARNING}not match case: {not_match} \n")
+from matplotlib.pyplot import plot
+layer = [1, 2, 3, 4, 5, 7, 8]
+y = np.array([[]])
+for x in layer:
+    name = 'freeze' + str(x)
+    print('number of freeze:', name, '==========')
+    result_df = pd.read_csv(name + '/test_identity_result.csv', header=0)
+    match = 0
+    not_match = 0
+    for i, row in result_df.iterrows():
+        if int(row['expected'] == row['actual']):
+            match = match + 1
+        else:
+            not_match += 1
+    # print(f"for negative method")
+    print(f" {Colors.OKGREEN}match case    :{match}")
+    print(f"{Colors.WARNING}not match case: {not_match} \n")
+    n = np.array([[x,match]])
+    y = np.append(y,n, axis=0)
+    print('==================================')
+plot(y)
+import matplotlib
+matplotlib.pyplot.show()
+"""
+for name in ['freeze8']:
+    print('number of freeze:', name, '==========')
+    result_df = pd.read_csv(name + '/test_identity_result.csv', header=0)
+    intent_df = pd.read_csv(path_common.intent_list.value, header=0)
+    match = 0
+    not_match = 0
+    for i, row in result_df.iterrows():
+        if row['expected'] == row['actual'] or row['expected'] == row['max2'] or row['expected'] == row['max3']:
+            match = match + 1
+        else:
+            not_match += 1.5
+    print(f"{Colors.ENDC}for positive method")
+    print(f"{Colors.OKGREEN}match case    :{match}")
+    print(f"{Colors.WARNING}not match case: {not_match} \n")
+"""
