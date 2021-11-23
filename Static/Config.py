ff48b435e59e6066f9ac6bbc175146d61fe4b455
duong-sau <duongsau1211@gmail.com>
2021-11-23 17:02:38 +0700

    "

--- Static/Config.py
+++ Static/Config.py
@@ -9,7 +9,7 @@ from Static.Define import PathCommon
 
 MODEL = {
     'name': 't5-small',
-    'data_link': "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/Data/IntentClassification"
+    'data_link': "https://raw.githubusercontent.com/duong-sau/chatbot1212/master/Model/ClassData/IntentClassification"
                  "/Positive/learn_data.csv",
     'num_freeze': [1, 1, 1, 0, 1, 0],
     'SEED': 1211
